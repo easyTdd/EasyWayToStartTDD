265a938311b0b7e4c73c765437f5925ecfbe90d7
kazys <kazys.rackauskas@gmail.com>
2023-08-06 18:59:09 +0300

    added FluentAssertion and empty test class

---
+++ ClassLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ClassLibrary.Tests
+{
+	public class CalculatorTests
+	{
+		private string _expression;
+
+		[SetUp]
+		public void Setup()
+		{
+			_expression = string.Empty;
+		}
+
+		private decimal CallCalculate()
+		{
+			var sut = Create();
+
+			return sut
+				.Calculate(
+					_expression
+				);
+		}
+
+		private Calculator Create()
+		{
+			return new Calculator();
+		}
+	}
+}
